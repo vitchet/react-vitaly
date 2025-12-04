5cbc63c8918e9fc52b9b12d10d278bb5c2bede1d
vitchet <94477735+vitchet@users.noreply.github.com>
2025-12-05 00:59:25 +0300

    Add css resets and main variables
    - Add reset
    - Add variables for colors
    - Add font size
    - Stop using scroll progress bar

--- src/components/app-layout/app-layout.jsx
+++ src/components/app-layout/app-layout.jsx
@@ -1,17 +1,8 @@
 import { ScrollProgressBar } from "../scroll-progres-bar/scroll-progress-bar";
-
-const headerStyles = {
-  position: "fixed",
-  width: "100%",
-  height: "1rem",
-};
-
 export const AppLayout = ({ children }) => {
   return (
     <>
-      <header style={headerStyles}>
-        <ScrollProgressBar />
-      </header>
+      <header></header>
       <main>{children}</main>
       <footer></footer>
     </>
