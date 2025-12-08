bca14c653f7ac25f15cd2d76fafd5e5f544ada54
vitchet <94477735+vitchet@users.noreply.github.com>
2025-12-08 22:36:08 +0300

    Style review form
    - Add semantic html to ReviewForm
    - Style ReviewForm
    - Style Review
    - Add forms and inputs to the reset

--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -1,6 +1,8 @@
+import styles from "./review-form.module.scss";
+
 import { useForm } from "./use-form";
 
-import { Button } from "../button/button";
+import { Button, ButtonSize } from "../button/button";
 import { Counter } from "../counter/counter";
 
 export const ReviewForm = () => {
@@ -16,33 +18,40 @@ export const ReviewForm = () => {
   } = useForm();
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <p>
+    <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
+      <section className={styles.inputPad}>
         <input
           type="text"
           placeholder="Enter your name..."
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
+          className={styles.nameInput}
         />
-      </p>
-      <p>
         <textarea
-          placeholder="Right your review..."
+          placeholder="Write your review..."
           value={text}
           onChange={(e) => setText(e.target.value)}
+          className={styles.textarea}
         />
-      </p>
-      <p>
-        <span>Rating: </span>
-        <Counter
-          count={rating}
-          increment={incrementRating}
-          decrement={decrementRating}
-        />
-      </p>
-      <Button type="button" value={text} onClick={clear}>
-        CLEAR
-      </Button>
+      </section>
+      <section className={styles.buttonPad}>
+        <p>
+          <span>Rating: </span>
+          <Counter
+            count={rating}
+            increment={incrementRating}
+            decrement={decrementRating}
+          />
+        </p>
+        <Button
+          type="button"
+          size={ButtonSize.SMALL}
+          value={text}
+          onClick={clear}
+        >
+          CLEAR
+        </Button>
+      </section>
     </form>
   );
 };
