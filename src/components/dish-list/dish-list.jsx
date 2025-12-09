3a2640bca75220df1c7d43c73177416ee4a6a432
vitchet <94477735+vitchet@users.noreply.github.com>
2025-12-09 14:48:37 +0300

    Update dish-list.jsx
    Remove unused closing tag

--- src/components/dish-list/dish-list.jsx
+++ src/components/dish-list/dish-list.jsx
@@ -3,10 +3,7 @@ import { List } from "../list/list";
 
 export const DishList = ({ dishes }) => {
   return dishes?.length > 0 ? (
-    <List
-      items={dishes}
-      itemComponent={(dish) => <Dish dish={dish}></Dish>}
-    ></List>
+    <List items={dishes} itemComponent={(dish) => <Dish dish={dish}></Dish>} />
   ) : (
     <p>There are no dishes available.</p>
   );
