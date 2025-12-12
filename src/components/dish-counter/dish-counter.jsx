5f2842b25c7d494ab25f34569fbc5881f23c1fb9
vitchet <94477735+vitchet@users.noreply.github.com>
2025-12-13 01:26:12 +0300

    Update dish-counter.jsx
    Reset counter on logging out

--- src/components/dish-counter/dish-counter.jsx
+++ src/components/dish-counter/dish-counter.jsx
@@ -1,12 +1,19 @@
 import { useCount } from "../../hooks/use-count";
 import { useContext } from "react";
+import { useEffect } from "react";
 
 import { Counter } from "../counter/counter";
 import { UserContext } from "../user-context/user-context";
 
 export const DishCounter = () => {
-  const { count, increment, decrement } = useCount();
+  const { count, increment, decrement, reset } = useCount();
   const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!user) {
+      reset();
+    }
+  }, [user]);
 
   if (!user) {
     return null;
