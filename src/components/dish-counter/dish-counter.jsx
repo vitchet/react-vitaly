957222b51b492933f9f5b6355c4e6ea1b5a138f6
vitchet <94477735+vitchet@users.noreply.github.com>
2025-12-13 01:21:52 +0300

    Update dish-counter.jsx
    Show DishCounter only if there is a user logged in

--- src/components/dish-counter/dish-counter.jsx
+++ src/components/dish-counter/dish-counter.jsx
@@ -1,8 +1,16 @@
 import { useCount } from "../../hooks/use-count";
+import { useContext } from "react";
+
 import { Counter } from "../counter/counter";
+import { UserContext } from "../user-context/user-context";
 
 export const DishCounter = () => {
   const { count, increment, decrement } = useCount();
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return null;
+  }
 
   return <Counter count={count} increment={increment} decrement={decrement} />;
 };
