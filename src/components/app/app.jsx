2688b36137cb6f6f16bfa4ce647fc166771c4394
vitchet <94477735+vitchet@users.noreply.github.com>
2025-12-18 18:49:19 +0300

    Add Redux
    - Add Redux Toolkit and React Redux to the project
    - Create store and provide it
    - Add normalized mock

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,15 +8,18 @@ import { AppLayout } from "../app-layout/app-layout";
 import { RestaurantPage } from "../restaurant-page/restaurant-page";
 import { ThemeContextProvider } from "../theme-context/theme-context-provider";
 import { UserContextProvider } from "../user-context/user-context-provider";
+import { store } from "../../entities/store";
 
 export const App = () => {
   return (
-    <ThemeContextProvider>
-      <UserContextProvider>
-        <AppLayout>
-          <RestaurantPage restaurants={restaurants} />
-        </AppLayout>
-      </UserContextProvider>
-    </ThemeContextProvider>
+    <Provider store={store}>
+      <ThemeContextProvider>
+        <UserContextProvider>
+          <AppLayout>
+            <RestaurantPage restaurants={restaurants} />
+          </AppLayout>
+        </UserContextProvider>
+      </ThemeContextProvider>
+    </Provider>
   );
 };
