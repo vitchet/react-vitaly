faf56a53ff09b2b5ccfdfb66cad973fbd4fdb10e
vitchet <94477735+vitchet@users.noreply.github.com>
2025-12-22 18:12:18 +0300

    Remove useAuth hook

--- src/components/auth-context/auth-context-provider.jsx
+++ src/components/auth-context/auth-context-provider.jsx
@@ -1,9 +1,42 @@
-import { useAuth } from "./use-auth";
+import { useDispatch } from "react-redux";
+import { useReducer } from "react";
 
+import { clear } from "@/redux/entities/cart/cart-slice";
+
+import Auth from "@/classes/Auth";
 import { AuthContext } from "./auth-context";
 
+const Action = Object.freeze({
+  SIGN_IN: Symbol("si"),
+  SIGN_OUT: Symbol("so"),
+});
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case Action.SIGN_IN: {
+      return new Auth(payload);
+    }
+    case Action.SIGN_OUT:
+      return null;
+    default:
+      return null;
+  }
+};
+
 export const AuthContextProvider = ({ children }) => {
-  const { auth, signIn, signOut } = useAuth();
+  const dispatchSliceAction = useDispatch();
+  const [auth, dispatch] = useReducer(reducer, null);
+
+  const signIn = (userName) => {
+    if (userName?.length) {
+      dispatch({ type: Action.SIGN_IN, payload: userName });
+    }
+  };
+
+  const signOut = () => {
+    dispatch({ type: Action.SIGN_OUT });
+    dispatchSliceAction(clear());
+  };
 
   return (
     <AuthContext value={{ auth, signIn, signOut }}>{children}</AuthContext>
